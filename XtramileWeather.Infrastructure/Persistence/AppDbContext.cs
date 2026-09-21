eed253a38d94ede234b14a1ae2e08e81dc9d5c2c
Moch. Sidik Andina Aris <moch.shidiq@gmail.com>
2026-09-22 00:06:09 +0700

    Add application database context

---
+++ XtramileWeather.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XtramileWeather.Domain.Entities;
+
+namespace XtramileWeather.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries => Set<Country>();
+
+        public DbSet<City> Cities => Set<City>();
+
+        public DbSet<FavoriteCity> FavoriteCities => Set<FavoriteCity>();
+    }
+}
