868aa91b6fbff8cc6e91a897bc636de5be42ffc1
Moch. Sidik Andina Aris <moch.shidiq@gmail.com>
2026-09-22 00:18:34 +0700

    Configure SQLite database connection

--- XtramileWeather.Api/Program.cs
+++ XtramileWeather.Api/Program.cs
@@ -1,3 +1,5 @@
+using XtramileWeather.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException(
+        "DefaultConnection is not configured.");
+
+builder.Services.AddInfrastructure(connectionString);
 
 var app = builder.Build();
 
---
+++ XtramileWeather.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using XtramileWeather.Infrastructure.Persistence;
+
+namespace XtramileWeather.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        string connectionString)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlite(connectionString));
+
+        return services;
+    }
+}
