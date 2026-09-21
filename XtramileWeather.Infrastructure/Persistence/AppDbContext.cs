85f8756797af0785ecdeea5b00de6e90dc4a286c
Moch. Sidik Andina Aris <moch.shidiq@gmail.com>
2026-09-22 00:12:22 +0700

    Configure entity relationships and constraints

--- XtramileWeather.Infrastructure/Persistence/AppDbContext.cs
+++ XtramileWeather.Infrastructure/Persistence/AppDbContext.cs
@@ -20,5 +20,59 @@ namespace XtramileWeather.Infrastructure.Persistence
         public DbSet<City> Cities => Set<City>();
 
         public DbSet<FavoriteCity> FavoriteCities => Set<FavoriteCity>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.Property(x => x.Code)
+                    .IsRequired()
+                    .HasMaxLength(2);
+
+                entity.Property(x => x.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.HasIndex(x => x.Code)
+                    .IsUnique();
+            });
+
+            modelBuilder.Entity<City>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.Property(x => x.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.HasOne(x => x.Country)
+                    .WithMany()
+                    .HasForeignKey(x => x.CountryId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasIndex(x => new { x.CountryId, x.Name })
+                    .IsUnique();
+            });
+
+            modelBuilder.Entity<FavoriteCity>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.HasOne(x => x.City)
+                    .WithMany()
+                    .HasForeignKey(x => x.CityId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasIndex(x => x.CityId)
+                    .IsUnique();
+
+                entity.Property(x => x.CreatedAtUtc)
+                    .IsRequired();
+            });
+        }
     }
 }
